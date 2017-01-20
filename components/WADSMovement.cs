5ef0320a0fff5d07ff3f03bfa6496a9d0415cdf6
Justin Basinger <justin.m.basinger@gmail.com>
2017-01-20 09:00:59 -0500

    Adding a WADS movement script and a couple models made by Lillian and myself

---
+++ components/WADSMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WADSMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = gameObject.transform.position;
+
+		if (Input.GetKey(KeyCode.W)) {
+			pos.z += speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.S)) {
+			pos.z -= speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			pos.x += speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			pos.x -= speed * Time.deltaTime;
+		}
+
+		gameObject.transform.position = pos;
+
+	}
+}
